f864745611920068ef43faae0ca66a1cff50a096
hammad-gik <72938592+hammad-gik@users.noreply.github.com>
2020-10-29 19:16:14 +0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world
+          Hello world how are you?
           Edit <code>src/App.js</code> and save to reload your web app.
         </p>
         <a
